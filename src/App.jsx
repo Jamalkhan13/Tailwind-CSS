4ea391d14577ef19c135551a6792b5db8a5a6b5b
Jamalkhan13 <jamalkhan7858@gmail.com>
2025-01-16 10:52:04 +0500

    Tailwind first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+const App = ()=>(
+
+  <main className="relative">
+    nav
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      Hero
+    </section>
+
+    <section className="padding">
+      popularProducts
+    </section>
+
+    <section className="padding">
+      SuperQuality
+    </section>
+
+    <section className="padding-x py-10">
+      Services
+    </section>
+
+    <section className="bg-pale-blue padding">
+      CustomersReviews
+    </section>
+
+    <section className=" padding-x sm:py-32 py-16 w-full">
+      Subscribe
+    </section>
+
+    <section className="bg-black padding-x padding-t pb-8">
+      Footer
+    </section>
+
+   
+  </main>
+)
+
+export default App;
