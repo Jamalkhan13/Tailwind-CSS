458ff2a331de2d04890b34b3b9d52ca29a2d111b
Jamalkhan13 <jamalkhan7858@gmail.com>
2025-01-16 15:10:08 +0500

    nike project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import { Hero,PopularProducts,SuperQuality,Services,SpecialOffers,CustomersReviews,Subscribe } from "./sections"
+import { Hero, PopularProducts, SuperQuality, Services, SpecialOffers, CustomerReviews, Subscribe, Footer } from "./sections";
 
 
-const App = ()=>(
-
+const App = () => (
   <main className="relative">
     {/* <nav/> */}
     <section className="xl:padding-1 wide:padding-r padding-b">
@@ -10,35 +9,33 @@ const App = ()=>(
     </section>
 
     <section className="padding">
-    <PopularProducts/>  
+      <PopularProducts />
     </section>
 
     <section className="padding">
-      <SuperQuality/>
+      <SuperQuality />
     </section>
 
     <section className="padding-x py-10">
-      <Services/>
+      <Services />
     </section>
 
     <section className="padding-x py-10">
-      <SpecialOffers/>
+      <SpecialOffers />
     </section>
 
     <section className="bg-pale-blue padding">
-      <CustomersReviews/>
+      <CustomerReviews />
     </section>
 
-    <section className=" padding-x sm:py-32 py-16 w-full">
-      <Subscribe/>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe />
     </section>
 
     <section className="bg-black padding-x padding-t pb-8">
-      <Footer/>
+      <Footer />
     </section>
-
-   
   </main>
-)
+);
 
 export default App;
--- src/sections/index.js
+++ src/sections/index.js
@@ -2,16 +2,18 @@ import Hero from "./Hero";
 import PopularProducts from "./popularProducts";
 import SuperQuality from "./superQuality";
 import Services from "./services";
-import SpecialOffers from "./specialOffers"
+import SpecialOffers from "./specialOffers";  // Make sure this is capitalized correctly
 import CustomerReviews from "./customerReviews";
 import Subscribe from "./Subscribe";
+import Footer from "./Footer";
 
-export {
-  Hero,
-  PopularProducts,
-  SuperQuality,
-  Services,
-  SpecialOffers,
-  CustomerReviews,
-  Subscribe,
+export { 
+  Hero, 
+  PopularProducts, 
+  SuperQuality, 
+  Services, 
+  SpecialOffers, 
+  CustomerReviews, 
+  Subscribe ,
+  Footer
 };
